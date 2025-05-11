7aeea23d71bf98a26a63264e60ed7743a353dd0f
samprasdsouza02 <sampras.dsouza@cimpress.com>
2025-05-11 19:40:26 +0530

    cuda enabled

--- sampras-test-scripts/pretrain.sh
+++ sampras-test-scripts/pretrain.sh
@@ -1,11 +1,11 @@
 # Set the path to save checkpoints
-OUTPUT_DIR='/shared/home/v_sampras_dsouza/my_scratch/VideoMAE/VideoMAE/k400_videomae_pretrain_base_patch16_224_frame_16x4_tube_mask_ratio_0.9_e100'
+OUTPUT_DIR='/Users/apple/personal/CMU-research/VideoMAE/k400_videomae_pretrain_base_patch16_224_frame_16x4_tube_mask_ratio_0.9_e100'
 # Set the path to Kinetics train set. 
-DATA_PATH='/shared/home/v_sampras_dsouza/my_scratch/shrec_21_video_dataset/train.csv'
+DATA_PATH='/Users/apple/personal/CMU-research/VideoMAE/shrec21_video_dataset/train.csv'
 
 # batch_size can be adjusted according to number of GPUs
 # this script is for 64 GPUs (8 nodes x 8 GPUs)
-OMP_NUM_THREADS=1 python ../run_mae_pretraining.py \
+CUDA_VISIBLE_DEVICES="0" python ../run_mae_pretraining.py \
         --data_path ${DATA_PATH} \
         --mask_type tube \
         --mask_ratio 0.9 \
