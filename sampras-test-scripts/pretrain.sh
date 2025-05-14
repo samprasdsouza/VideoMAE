e70e984b08cc81b0b84fd56ac6c6febaa9abf35e
samprasdsouza02 <sampras.dsouza@cimpress.com>
2025-05-15 01:45:33 +0530

    nohup + more epochs

--- sampras-test-scripts/pretrain.sh
+++ sampras-test-scripts/pretrain.sh
@@ -5,7 +5,7 @@ DATA_PATH='/shared/scratch/0/home/v_sampras_dsouza/shrec_21_video_dataset/train.
 
 # batch_size can be adjusted according to number of GPUs
 # this script is for 64 GPUs (8 nodes x 8 GPUs)
-python ../run_mae_pretraining.py \
+nohup python ../run_mae_pretraining.py \
         --data_path ${DATA_PATH} \
         --mask_type tube \
         --mask_ratio 0.9 \
@@ -18,7 +18,7 @@ python ../run_mae_pretraining.py \
         --opt_betas 0.9 0.95 \
         --warmup_epochs 2 \
         --save_ckpt_freq 20 \
-        --epochs 11 \
+        --epochs 21 \
         --weight_decay 0.05\
         --log_dir ${OUTPUT_DIR} \
         --output_dir ${OUTPUT_DIR}
